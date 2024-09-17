dd58a4d063be4c16acfcc9b3b12a5ec995f253f9
brayden <jakebra000@warren.k12.in.us>
2024-09-16 20:42:27 -0400

    done

---
+++ main.py
@@ -0,0 +1,6 @@
+frisbees = 100 #pretend we got from user input
+COST_OF_FRISBEES = 500
+total_cost =50000 #YOU DO THIS PART WITH VARIABLES AND MATH OPS
+print("The total cost is $", total_cost, sep="")
+print("COST_OF_FRISBEES each  500$")
+print("I want to buy 100 FRISBEES" )
